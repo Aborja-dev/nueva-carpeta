384b4beac162e2112743b0abe65d8618be2bb7de
ankahara <hecatombe100@gmail.com>
2024-08-13 01:21:48 -0600

    install tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ function App() {
 
   return (
     <>
-    <main className="container">
-      <h1>Hello world!</h1>
-    </main>
+      <main className="container">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </main>
     </>
   )
 }
